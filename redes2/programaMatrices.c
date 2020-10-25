57b59190bed8d1065f0b464e76ffd9b13f2d0da7
Emiliano Perez <emilianoparrot@localhost.localdomain>
2020-10-25 14:22:26 -0500

    poblar matrices con valores aleatorios y mostrarla

--- redes2/programaMatrices.c
+++ redes2/programaMatrices.c
@@ -8,6 +8,9 @@
 int verificarInput(int input);
 int verificarHilos(int n, int col, int row);
 int verificarMatrices(int col, int row);
+int **crearMatriz(int row, int col);
+int **poblarMatrices(int row, int col, int **Matriz);
+void imprimirMatriz(int row, int col, int **Matriz);
 
 int main(int argc, char *argv[]) {
   if (verificarInput(argc) == -1)
@@ -22,10 +25,17 @@ int main(int argc, char *argv[]) {
     return -1;
   if (verificarMatrices(columnas_A, filas_B) == -1)
     return -1;
+  int **A = crearMatriz(filas_A, columnas_A);
+  int **B = crearMatriz(filas_B, columnas_B);
+  int **C = crearMatriz(filas_A, columnas_B);
   printf("Se realizara la multiplicacion de matrices con los valores:\n");
   printf("Columnas de A: %d\nFilas de A: %d\n", columnas_A, filas_A);
   printf("Columnas de B: %d\nFilas de B: %d\n", columnas_B, filas_B);
   printf("Numero de hilos: %d\n", numHilos);
+  A = poblarMatrices(filas_A, columnas_B, A);
+  printf("Matriz A poblada\n");
+  B = poblarMatrices(filas_B, columnas_B, B);
+  printf("Matriz B poblada\n");
 }
 
 int verificarInput(int input) {
@@ -58,4 +68,38 @@ int verificarMatrices(int col, int row) {
     return -1;
   }
   return 0;
+}
+
+int **crearMatriz(int row, int col) {
+  int **Matriz = (int **)malloc(row * sizeof(int *));
+  for (int i = 0; i < row; i++) {
+    Matriz[i] = (int *)malloc(col * sizeof(int));
+  }
+  return Matriz;
+}
+
+int **poblarMatrices(int row, int col, int **Matriz) {
+  int max_rand = 5;
+  for (int i = 0; i < row; i++) {
+    for (int j = 0; j < col; j++) {
+      Matriz[i][j] = rand() % max_rand + 1;
+    }
+  }
+  imprimirMatriz(row, col, Matriz);
+  return Matriz;
+}
+
+void imprimirMatriz(int row, int col, int **Matriz) {
+  for (int i = 0; i < row; i++) {
+    for (int j = 0; j < col; j++) {
+      if (j == 0)
+        printf("| %d\t", Matriz[i][j]);
+
+      else if (j == col - 1)
+        printf("\t%d |\n", Matriz[i][j]);
+
+      else
+        printf(" %d ", Matriz[i][j]);
+    }
+  }
 }
