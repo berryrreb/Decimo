7735a2f8ce056d9f9b92c63fb543542ba6099987
Emiliano Perez <emilianoparrot@localhost.localdomain>
2020-10-26 10:00:01 -0500

    fix como que se crean y muestran matrices

--- redes2/programaMatrices.c
+++ redes2/programaMatrices.c
@@ -9,7 +9,7 @@ int verificarInput(int input);
 int verificarHilos(int n, int col, int row);
 int verificarMatrices(int col, int row);
 int **crearMatriz(int row, int col);
-int **poblarMatrices(int row, int col, int **Matriz);
+int **poblarMatriz(int row, int col, int **Matriz);
 void imprimirMatriz(int row, int col, int **Matriz);
 
 int main(int argc, char *argv[]) {
@@ -28,21 +28,23 @@ int main(int argc, char *argv[]) {
   int **A = crearMatriz(filas_A, columnas_A);
   int **B = crearMatriz(filas_B, columnas_B);
   int **C = crearMatriz(filas_A, columnas_B);
-  printf("Se realizara la multiplicacion de matrices con los valores:\n");
-  printf("Columnas de A: %d\nFilas de A: %d\n", columnas_A, filas_A);
-  printf("Columnas de B: %d\nFilas de B: %d\n", columnas_B, filas_B);
-  printf("Numero de hilos: %d\n", numHilos);
-  A = poblarMatrices(filas_A, columnas_B, A);
-  printf("Matriz A poblada\n");
-  B = poblarMatrices(filas_B, columnas_B, B);
-  printf("Matriz B poblada\n");
+  printf("[+] - Se realizara la multiplicacion de matrices con los valores:\n");
+  printf("[-] - Columnas de A: %d\n[-] - Filas de A: %d\n", columnas_A,
+         filas_A);
+  printf("[-] - Columnas de B: %d\n[-] - Filas de B: %d\n", columnas_B,
+         filas_B);
+  printf("[-] - Numero de hilos: %d\n\n", numHilos);
+  printf("\n[*] - Matriz A poblada\n\n");
+  A = poblarMatriz(filas_A, columnas_A, A);
+  printf("\n[*] - Matriz B poblada\n\n");
+  B = poblarMatriz(filas_B, columnas_B, B);
 }
 
 int verificarInput(int input) {
   if (input < 5) {
     printf("[ERROR] - Error al introducir datos\n");
     printf("[ERROR] - Numero de parametros incorrecto\n");
-    printf("Uso: ./programa <colA> <filA> <colB> <filB> <numHilos>\n");
+    printf("Uso correcto: ./programa <colA> <filA> <colB> <filB> <numHilos>\n");
     return -1;
   }
   return 0;
@@ -78,7 +80,7 @@ int **crearMatriz(int row, int col) {
   return Matriz;
 }
 
-int **poblarMatrices(int row, int col, int **Matriz) {
+int **poblarMatriz(int row, int col, int **Matriz) {
   int max_rand = 5;
   for (int i = 0; i < row; i++) {
     for (int j = 0; j < col; j++) {
@@ -92,14 +94,30 @@ int **poblarMatrices(int row, int col, int **Matriz) {
 void imprimirMatriz(int row, int col, int **Matriz) {
   for (int i = 0; i < row; i++) {
     for (int j = 0; j < col; j++) {
-      if (j == 0)
-        printf("| %d\t", Matriz[i][j]);
-
-      else if (j == col - 1)
-        printf("\t%d |\n", Matriz[i][j]);
-
-      else
-        printf(" %d ", Matriz[i][j]);
+      printf("%d\t", Matriz[i][j]);
+      if (j == col - 1) {
+        printf("\n");
+      }
     }
   }
+  // if (row == 1 & col == 1)
+  //   printf("| %d |\n", Matriz[0][0]);
+  // else {
+  //   for (int i = 0; i < row; i++) {
+  //     for (int j = 0; j < col; j++) {
+  //       if(col == 1) printf("| %d |\n", Matriz[i][j]);
+  //       else if (j == 0)
+  //         printf("| %d\t", Matriz[i][j]);
+
+  //       else if (j == col - 1)
+  //         printf("\t%d |\n", Matriz[i][j]);
+
+  //       else
+  //         printf(" %d ", Matriz[i][j]);
+  //     }
+  //     if (i == row - 1) {
+  //       printf("\n");
+  //     }
+  //   }
+  // }
 }
